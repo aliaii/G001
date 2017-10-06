637dd2aa6059830f297b97f5338a06c16965f15f
aliaii <32382343+aliaii@users.noreply.github.com>
2017-10-06 15:27:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var http = require('http');
+
+ var http = require('http');
 
  http.createServer((request, response) => {
  let fs = require('fs')
 
  fs.readFile('index.html', (err, data) => {
  response.writeHead(200, {
-'Content-Type': 'text/html'
+ 'Content-Type': 'text/html'
  });
  response.write(data);
  response.end();
-});
- }).listen(8088);
+ });
+ }).listen(8081);
 
- // log message to Console
- console.log('Server running at http://127.0.0.1:8088/');
+// log message to Console
+ console.log('Server running at http://127.0.0.1:8081/');
